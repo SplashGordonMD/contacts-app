e65e8f9c0a4082c6e9cdb711bf3c6bb7ddf9a469
Casey Platt <kse0824@gmail.com>
2016-11-16 11:47:57 -0600

    1147am111616

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,32 @@
 class ContactsController < ApplicationController
-  def email
-    @contact = Contact.find_by(email: "Tom@uplink.com")
+  def index
+    @contacts = Contact.all
     render "contact.html.erb"
   end 
-  def index
-    @contacts=Contact.all
-  render "index.html.erb"
- end
+  
+  def new
+    render "new.html.erb"
+  end
 
+  def create
+    message = Message.new(name: params[:name], address: params[:address])
+    message.save
+    render "create.html.erb"    
+  end
+
+  def show
+    render "show.html.erb"
+  end
+
+  def edit
+    render "edit.html.erb"
+  end
+
+  def update
+    render "update.html.erb"
+  end
+
+  def destroy
+    render "update.html.erb"
+  end
 end
